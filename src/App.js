bc2da3fb7c1c5758bf7b6bcf905aa23dab72f73a
Tamika226 <tashysoganar@gmail.com>
2021-01-28 23:39:36 +0700

    Не получилось разобраться с bgUrl и bgColor, поняла, что их нужно задавать в самом компоненте, но как именно? стилями не вышло, дальше соображайки не хватило. И вопрос, как по-умному задавать id, не руками же они в апп перечисляются

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Layout from "./components/layout";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello,React JS
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header title="This is title" description="This is Description!"/>
+            <Layout title="This is title" description="This is Description!" id="1" urlBg colorBg/>
+            <Layout title="This is title" description="This is Description!" id="2" urlBg colorBg hideBackground/>
+            <Layout title="This is title" description="This is Description!" id="3" urlBg colorBg/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
